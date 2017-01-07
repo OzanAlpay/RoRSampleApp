6736e573439f03396e1b4583d17e4b71e6988e1a
Ozan Alpay <ozanalpay@yandex.com.tr>
2017-01-07 19:27:06 +0000

    Finish layout and routes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,8 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  include ApplicationHelper
+  
 
   # Add more helper methods to be used by all tests here...
 end
